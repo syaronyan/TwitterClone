d0d2b7fb8e272ea4fd8031beec5803d5d9e9cf2e
syaronyan <navybeeshrimp@gmail.com>
2021-06-02 18:11:14 +0900

    会員登録画面、ログイン画面実装

---
+++ Views/sign-in.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/logo-twitterblue.svg">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>ログイン画面 / Twitterクローン</title>
+    <meta name="description" content="ログイン画面です">
+</head>
+<body class="signup text-center">
+    <main class="form-signup">
+        <form action="sign-in.php" method="post">
+            <img src="img/logo-white.svg" alt="" class="logo-white">
+            <h1>Twitterクローンにログイン</h1>
+            <input type="email" class="form-control" name="email" placeholder="メールアドレス" required autofocus>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" required>
+            <button class="w-100 btn btn-lg" type="submit">ログイン</button>
+            <p class="mt-3 mb-2"><a href="sign-up.php">会員登録する</a></p>
+            <p class="mt-2 mb-3 text-muted">&copy; 2021</p>
+        </form>
+    </main>
+</body>
+</html>
